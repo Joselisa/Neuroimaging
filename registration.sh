67a6061eb576c03dcf173465254c49eada0838d1
Joselisa <joselisa@gmail.com>
2018-02-16 16:13:16 -0200

    To be used after bedpostx (when lacking the xfms folder)

--- registration.sh
+++ registration.sh
@@ -7,21 +7,20 @@ for subjdir in *;
  do
     echo Running "$subjdir"... ;
         
-	mkdir -p ${subjdir}.bedpostX/xfms
+	mkdir -p ${subjdir}/xfms
 	echo Created xfms folder "$subjdir"
 
 	flirt -in /Users/INCE/Documents/Bedpostx/${subjdir}/nodif_brain -ref /usr/local/fsl/data/standard/FMRIB58_FA_1mm.nii.gz -omat /Users/INCE/Documents/Bedpostx/${subjdir}/xfms/diff2standard.mat -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12
 
 	echo Flirt ok!;
-	fnirt --in=/Users/INCE/Documents/Bedpostx/${subjdir}/nodif_brain --aff=/Users/INCE/Documents/Bedpostx/${subjdir}/xfms/diff2standard.mat --ref=/usr/local/fsl/data/standard/FMRIB58_FA_1mm.nii.gz --cout=diff2standard_warp
+	fnirt --in=/Users/INCE/Documents/Bedpostx/${subjdir}/nodif_brain --aff=/Users/INCE/Documents/Bedpostx/${subjdir}/xfms/diff2standard.mat --ref=/usr/local/fsl/data/standard/FMRIB58_FA_1mm.nii.gz --cout=/Users/INCE/Documents/Bedpostx/${subjdir}/xfms/diff2standard_warp
 	echo FNIRT ok!;
 	
 	convert_xfm -omat /Users/INCE/Documents/Bedpostx/${subjdir}/xfms/standard2diff.mat -inverse /Users/INCE/Documents/Bedpostx/${subjdir}/xfms/diff2standard.mat
 	echo diff2standard ok!;
-	
-	convert_xfm -omat /Users/INCE/Documents/Bedpostx/${subjdir}/xfms/standard2diff_warp -inverse /Users/INCE/Documents/Bedpostx/${subjdir}/xfms/diff2standard_warp
+
+	invwarp -w /Users/INCE/Documents/Bedpostx/${subjdir}/xfms/diff2standard_warp -o /Users/INCE/Documents/Bedpostx/${subjdir}/xfms/standard2diff_warp -r /usr/local/fsl/data/standard/FMRIB58_FA_1mm.nii.gz
+
 	echo diff2standard_warp ok!;
 
  done
-
-
